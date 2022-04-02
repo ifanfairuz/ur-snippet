52cb7cfa1487bedb160d5e334f445f8ce227a666
Ifan Fairuz <ifanfairuz@gmail.com>
2022-04-02 15:18:29 +0700

    [FIX] copied

--- src/app/copyclip.directive.ts
+++ src/app/copyclip.directive.ts
@@ -1,16 +1,33 @@
-import { Directive, HostListener, Input } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appCopyclip]'
+  selector: '[copy-clip]'
 })
 export class CopyclipDirective {
 
-  @Input() appCopyclip = '';
+  @Input('copy-clip') copyClip = '';
+  @Input('copied-label') copiedSelector: string|undefined = undefined;
+  @Input('copied-style') copiedStype: string = 'block';
 
-  constructor() { }
+  constructor(private el: ElementRef<HTMLElement>) { }
 
   @HostListener('click') onClick() {
-    navigator.clipboard.writeText(`\n${this.appCopyclip}\n\n`);
+    navigator.clipboard.writeText(`\n${this.copyClip}\n\n`);
+    this.callback();
+  }
+
+  private getCopiedLabelComp() {
+    return this.copiedSelector ? document.body.querySelector<HTMLElement>(this.copiedSelector) : undefined;
+  }
+
+  callback() {
+    const label = this.getCopiedLabelComp();
+    if (label) {
+      label.style.setProperty('display', this.copiedStype);
+      setTimeout(() => {
+        label.style.setProperty('display', 'none');
+      }, 1000);
+    }
   }
 
 }
